2cf62d7a9f8ed6c11df66051cdf3735e7c70fff4
lukmak0394 <lukmak0394@wp.pl>
2021-12-29 15:45:52 +0100

    first save

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('dziala')
+
+function getData() {
+    fetch('https://danepubliczne.imgw.pl/api/data/synop')
+        .then(res => res.json())
+        .then(data => {
+            
+            let row = document.querySelector('.row');
+            for (let i = 0; i < data.length; i++) {
+                let column = document.createElement('div');
+                column.classList.add('col-2');
+                column.classList.add('py-3');
+                column.classList.add('border');
+                column.classList.add('border-secondary');
+
+            };
+        });
+};
+
+getData();
